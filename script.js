4010eaf09ffb65b0beb5bc27835c8a2591e648ca
aswin <aswinkashok0@gmail.com>
2021-10-30 13:48:42 +0530

    variable_types

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
-
-var person = "smith";  // javascript will auto detect the types of variables and this is a string variable
+var person = "smith"; // javascript will auto detect the types of variables and this is a string variable
 
 document.write(person);
 document.write("<br>");
-document.write (typeof(person));
+document.write(typeof (person)); // to print the type of the given variable
+document.write("<br>");
+
+
+var number = 10;
+var number2 = number + 20.23;
+document.write(number);
+document.write("<br>");
+document.write(typeof (number, number2));
+document.write("<br> <br>");
+
+var tru =true;
+var fls =false;
+document.write(tru);
+document.write("-",typeof(tru),"<br>");
+document.write(fls);
+document.write("-",typeof(fls));
+
+var undifine;
+document.write("<br>",undifine,"-",typeof(undifine));
+
+var not = null;
+document.write("<br>",not + "<br>")
+
+const pi =3.14;             //constant variable - it one time declarable and the value wont change after that
+document.write(pi);
+
