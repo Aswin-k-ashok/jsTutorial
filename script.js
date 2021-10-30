cbd42c58e401178029b5b034d043152e2e74b9b0
aswin <aswinkashok0@gmail.com>
2021-10-30 13:58:48 +0530

    arithmatic_operators

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-var person = "smith"; // javascript will auto detect the types of variables and this is a string variable
+var a=10;
+var b=20;
 
-document.write(person);
-document.write("<br>");
-document.write(typeof (person)); // to print the type of the given variable
-document.write("<br>");
-
-
-var number = 10;
-var number2 = number + 20.23;
-document.write(number);
-document.write("<br>");
-document.write(typeof (number, number2));
-document.write("<br> <br>");
-
-var tru =true;
-var fls =false;
-document.write(tru);
-document.write("-",typeof(tru),"<br>");
-document.write(fls);
-document.write("-",typeof(fls));
-
-var undifine;
-document.write("<br>",undifine,"-",typeof(undifine));
-
-var not = null;
-document.write("<br>",not + "<br>")
-
-const pi =3.14;             //constant variable - it one time declarable and the value wont change after that
-document.write(pi);
-
+document.write(a+b,"<br>"); //addition
+document.write(a-b,"<br>"); //subtraction
+document.write(a*b,"<br>"); //multiplication
+document.write(a/b,"<br>"); //division
+document.write(a%b,"<br>"); //modules
+document.write(a++,"<br>"); //increment
+document.write(a--,"<br>"); //decrement
+document.write(b**2,"<br>"); //exponet
